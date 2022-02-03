d008d0b214081c1b0525d4d1ee014f6436cf799f
Benny623 <bmilner88@gmail.com>
2022-02-03 15:49:00 -0700

    adjusted letters and started generatePassword function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var specialChars = ['!', '@', '$', '%', '^', '&', '*'];
-var letters = [ 'a', 'A',
-                'b', 'B', 
-                'c', 'C', 
-                'd', 'D', 
-                'e', 'E', 
-                'f', 'F', 
-                'g', 'G', 
-                'h', 'H', 
-                'i', 'I', 
-                'j', 'J', 
-                'k', 'K', 
-                'l', 'L', 
-                'm', 'M', 
-                'n', 'N', 
-                'o', 'O', 
-                'p', 'P', 
-                'q', 'Q', 
-                'r', 'R', 
-                's', 'S', 
-                't', 'T', 
-                'u', 'U', 
-                'v', 'V', 
-                'w', 'W', 
-                'x', 'X', 
-                'y', 'Y', 
-                'z', 'Z', ]
+var lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
 
 // generatePassword() function
+var generatePassword = function() {
+  var includeSpecChars = window.confirm('Would you like to use special characters?')
+  if (includeSpecChars) {
 
+  }
+
+  var numOfChars = window.prompt('How many characters? (Must be at least 8 but no more than 128)')
+  numOfChars = parseInt(numOfChars);
+  // checking if input is a number and not below 8 or above 128
+  if (isNaN(numOfChars)) {
+    alert('Must be a number.');
+    generatePassword();
+  } else if (numOfChars < 8) {
+    alert('Must be at least 8 characters long.');
+    generatePassword();
+  } else if (numOfChars > 128) {
+    alert('Must not be longer than 128 characters.');
+    generatePassword();
+  };
+
+
+};
 
 // Write password to the #password input
 function writePassword() {
