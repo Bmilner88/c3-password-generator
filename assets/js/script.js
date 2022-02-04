3d79aab643d1f4057223b8253f06aaffbd06928a
Benny623 <bmilner88@gmail.com>
2022-02-04 12:47:20 -0700

    adjusted spacing

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // Character arrays
-var special = ['!', '@', '#', '$', '%', '^', '&', '*',  '/', '<', '>', '~'],
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '/', '<', '>', '~'],
     numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
     lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
     upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
